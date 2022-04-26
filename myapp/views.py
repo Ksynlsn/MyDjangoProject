126a1494f0dabf51c91e800189cffdca79ff64af
Ksynlsn <ksynlsn@gmail.com>
2022-04-26 11:22:44 -0700

    Rescource View Commit (assignment 5)

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
 def index(request):
     return render(request, 'myapp/index.html')
+
+
+def getresource(request):
+    resource_list=Resource.objects.all()
+    return render(request, 'myapp/resources.html', {'resource_list' : resource_list})
+
