45e687dc868995d3e0171d2dd758b00d7372093e
Ksynlsn <ksynlsn@gmail.com>
2022-04-28 13:09:11 -0700

    Assignment 6 Commit (list view)

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
    path('', views.index, name='index'),
    path('getresource/', views.getresource, name='resources'),
+   path('getmeeting/', views.getmeeting, name='meetings'),
+   path('meetingview/<int:id>', views.meetingview, name='details'),
 
 ]
