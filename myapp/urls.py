e9d85ae079e6054281d9f987bf5c952301572ae2
Ksynlsn <ksynlsn@gmail.com>
2022-05-12 13:48:38 -0700

    Form Commit/ add resourceview

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,10 @@ from . import views
 urlpatterns = [
    path('', views.index, name='index'),
    path('getresource/', views.getresource, name='resources'),
+   path('resourceview/<int:id>', views.resourceview, name='resource_details'),
    path('getmeeting/', views.getmeeting, name='meetings'),
    path('meetingview/<int:id>', views.meetingview, name='details'),
+   path('newmeeting/', views.newMeeting, name='newmeeting'),
+   path('newresource/', views.newResource, name='newresource'),
 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,22 +1,56 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Meeting, MeetingMinutes, Resource, Event
 from django.urls import reverse_lazy
+from .forms import MeetingForm, ResourceForm
 
 # Create your views here.
 def index(request):
     return render(request, 'myapp/index.html')
 
-
 def getresource(request):
     resource_list=Resource.objects.all()
     return render(request, 'myapp/resources.html', {'resource_list' : resource_list})
 
+def resourceview(request, id):
+    resource=get_object_or_404(Resource, pk=id)
+    return render(request, 'myapp/resourceview.html', {'resource' : resource})
 
 def getmeeting(request):
     meeting_list=Meeting.objects.all()
     return render(request, 'myapp/meetings.html', {'meeting_list' : meeting_list})
 
-
 def meetingview(request, id):
     meeting=get_object_or_404(Meeting, pk=id)
     return render(request, 'myapp/meetingview.html', {'meeting' : meeting})
+
+# form views
+def newMeeting(request):
+    form=MeetingForm
+
+    if request.method=='POST':
+        form=MeetingForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=True)
+            post.save()
+            form=MeetingForm()
+
+    else:
+        form=MeetingForm()
+
+    return render(request, 'myapp/newmeeting.html', {'form': form})  
+
+
+def newResource(request):
+    form=ResourceForm
+
+    if request.method=='POST':
+        form=ResourceForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=True) 
+            post.save()
+            form=ResourceForm()  
+
+    else:
+        form=ResourceForm()
+
+    return render(request, 'myapp/newresource.html', {'form': form})           
